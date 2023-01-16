56088b0924d32de9ac984a8c71093683b204b08e
sreech465 <surojusreechandra2@gmail.com>
2023-01-16 09:03:39 +0530

    first commit

---
+++ .history/traffic2_20230116000850.js
@@ -0,0 +1,39 @@
+const red = document.querySelector("#red");
+console.log(red);
+const orange = document.querySelector("#orange");
+const green = document.querySelector("#green");
+
+function changeLights() {
+  setTimeout(() => {
+    redLight();
+  }, 0);
+  setTimeout(() => {
+    orangeLight();
+  }, 4000);
+  setTimeout(() => {
+    greenLight();
+  }, 7000);
+}
+
+function redLight() {
+  red.classList.add("active");
+  setTimeout(() => {
+    red.classList.remove("active");
+  }, 4000);
+}
+
+function orangeLight() {
+  orange.classList.add("active");
+  setTimeout(() => {
+    orange.classList.remove("active");
+  }, 2000);
+}
+
+function greenLight() {
+  green.classList.add("active");
+  setTimeout(() => {
+    green.classList.remove("active");
+  }, 3000);
+}
+
+setInterval(changeLights, 9000);
---
+++ .history/traffic2_20230116083948.js
@@ -0,0 +1,39 @@
+const red = document.querySelector("#red");
+console.log(red);
+const yellow = document.querySelector("#yellow");
+const green = document.querySelector("#green");
+
+function changeLights() {
+  setTimeout(() => {
+    redLight();
+  }, 0);
+  setTimeout(() => {
+    yellowLight();
+  }, 4000);
+  setTimeout(() => {
+    greenLight();
+  }, 7000);
+}
+
+function redLight() {
+  red.classList.add("active");
+  setTimeout(() => {
+    red.classList.remove("active");
+  }, 4000);
+}
+
+function yellowLight() {
+  yellow.classList.add("active");
+  setTimeout(() => {
+    yellow.classList.remove("active");
+  }, 2000);
+}
+
+function greenLight() {
+  green.classList.add("active");
+  setTimeout(() => {
+    green.classList.remove("active");
+  }, 3000);
+}
+
+setInterval(changeLights, 9000);
